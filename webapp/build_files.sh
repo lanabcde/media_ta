b43b4248b14fa37f1fa0646c7d9122caf79ddc05
lanabcde <144417094+lanabcde@users.noreply.github.com>
2024-05-01 00:50:22 +0800

    Update build_files.sh

--- webapp/build_files.sh
+++ webapp/build_files.sh
@@ -1,2 +1,13 @@
-pip install -r requirements.txt 
-python3.10 manage.py collectstatic
+#!/bin/bash
+
+# Buat virtual environment jika belum ada
+python3.9 -m venv myenv
+
+# Aktifkan virtual environment
+source env/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Jalankan collectstatic
+python manage.py collectstatic
